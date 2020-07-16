ee8be08d8023f929e9dcfa533f6c7f3c0d529f3b
martin-505 <martin.gonzalez.2800@gmail.com>
2020-07-16 17:09:07 -0500

    i

---
+++ tarea.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+# primer tarea
+tarea(){
+        echo "El sat esta perrote "
+        wget http://omawww.sat.gob.mx/cifras_sat/Documents/Listado_Completo_69-B.csv
+        python script.py 
+}
+
+tarea
